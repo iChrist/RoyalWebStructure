af04123e9b1333ca1f1e776397fc963e5e6786df
iChrist <christianjimenezcjs@gmail.com>
2015-10-06 01:15:34 -0500

    modulo ejemplo areas

--- sys/emp/controller/emp.controller.php
+++ sys/emp/controller/emp.controller.php
@@ -1,31 +1,90 @@
 <?php
-	require_once(SYS_PATH.$_GET["sysModule"]."/model/".$_GET["sysModule"].".model.php");
-	class Emp_Controller extends Emp_Model {
+	require_once(SYS_PATH."emp/model/emp.model.php");
+	Class Emp_Controller Extends Emp_Model {
 
 		// PRIVATE VARIABLES //
-			private $_data = array();
+                    private $data = array();
 
 		public function __construct(){
 			parent::__construct();
 		}
 
 		public function __destruct(){
-			if($this->is_view_required()){
-				$this->load_view($_GET["sysFunction"], $this->_data);
-			}
+		
 		}
 
-		public function con_empr_con(){
-				/*$this->require_view();
-				$this->_data['modulos']= $this->GetConsMod(); 
-				$this->_data['empresas']=$this->GetConsEmp(); */
-				//$this->load_view('index',array(),TRUE);
-			//	$this->_data= parent::GetConsEmp(); 
-			//	$this->_data= Emp_Model::GetConsEmp(); 
-			
-			
-		}
-	 
-	 
+                /* COMIENZA MODULO areas */
+                public function areas_index(){
+                    if(isset($_GET['axn']) && $_GET['axn'] == 'fetch_all'){
+                        
+                        // PARAMETROS PARA FILTRADO //
+                        if(isset($_POST['nombre'])){
+                            $this->users['nombre'] = $_POST['nombre'];
+                        }
+                        if(isset($_POST['correo'])){
+                            $this->users['correo'] = $_POST['correo'];
+                        }
+                        if(isset($_POST['skStatus'])){
+                            $this->users['skStatus'] = $_POST['skStatus'];
+                        }
+                        
+                        // TOTAL DE REGISTROS EN LA TABLA //
+                        $getTotal = parent::count_areas()->fetch_assoc();
+                        $iTotalRecords = $getTotal['total'];
+                        // "LIMIT" TOTAL DE REGISTROS PARA MOSTRAR //
+                        $iDisplayLength = intval($_REQUEST['length']);
+                        $iDisplayLength = ($iDisplayLength < 0) ? $iTotalRecords : $iDisplayLength; 
+                        // "OFFSET" //
+                        $iDisplayStart = intval($_REQUEST['start']);
+                        // PAGINA //
+                        $sEcho = intval($_REQUEST['draw']);
+                        
+                        $this->users['limit'] = $iDisplayLength;
+                        $this->users['offset'] = $iDisplayStart;
+                        $this->data['areas'] = parent::read_like_areas();
+                        
+                        $records = array();
+                        $records["data"] = array(); 
+
+                        $end = $iDisplayStart + $iDisplayLength;
+                        $end = $end > $iTotalRecords ? $iTotalRecords : $end;
+                        
+                        if($this->data['areas']){
+                            while($row = $this->data['areas']->fetch_assoc()){
+                                $actions = $this->printModulesButtons(2,array($row['skUsers']));
+                                $records["data"][] = array(
+                                    htmlentities(utf8_encode($row['nombre']), ENT_QUOTES)
+                                    ,htmlentities(utf8_encode($row['correo']), ENT_QUOTES)
+                                    ,utf8_encode($row['htmlStatus'])
+                                    , !empty($actions['sHtml']) ? '<div class="dropdown"><button aria-expanded="true" aria-haspopup="true" data-toggle="dropdown" id="dropdownMenu1" type="button" class="btn btn-default btn-xs dropdown-toggle">Acciones<span class="caret"></span></button><ul aria-labelledby="dropdownMenu1" class="dropdown-menu">'.$actions['sHtml'].'</ul></div>' : ''
+                                    
+                                );
+                            }
+                        }
+                        $records["draw"] = $sEcho;
+                        $records["recordsTotal"] = $iTotalRecords;
+                        $records["recordsFiltered"] = $iTotalRecords;
+
+                        echo json_encode($records);
+                        return false;
+                    }
+                    
+                    // INCLUYE UN MODELO DE OTRO MODULO //
+                    $this->load_model('cof','cof');
+                    $this->data['status'] = Cof_Model::read_status();
+                    
+                    // RETORNA LA VISTA areas-index.php //
+                    $this->load_view('areas-index', $this->data);
+                }
+                
+                public function areas_form(){
+                    
+                }
+                
+                public function areas_detail(){
+                    
+                }
+                /* TERMINA MODULO areas */
+                
 	}
 ?>
--- sys/emp/model/emp.model.php
+++ sys/emp/model/emp.model.php
@@ -1,24 +1,148 @@
 <?php
-	abstract class Emp_Model extends Core_Model {
-	
-		// PRIVATE VARIABLES //
-			private $_data = array();
+	Abstract Class Emp_Model Extends Core_Model {
+                
+            // PUBLIC VARIABLES //
+                public $areas = array(
+                    'skAreas'       =>  ''
+                    ,'nombre'       =>  ''
+                    ,'correo'       =>  ''
+                    ,'skStatus'     =>  ''
+                    ,'limit'        =>  ''
+                    ,'offset'       =>  ''
+                );
+                    
+            // PRIVATE VARIABLES //
+                    private $data = array();
 
-		public function __construct(){
-			parent::__construct();
-		}
+            public function __construct(){
+                    parent::__construct();
+            }
 
-		public function __destruct(){
-			
-		}
+            public function __destruct(){
 
-		public function GetConsMod(){
-			// OPERACIONES A BASE DE DATOS //
-			$sql = "SELECT * FROM _modules";
-			//echo $sql;
-			$result = $this->db->query($sql);
-			return $result;
-		}
-
+            }
+            
+            /* COMIENZA MODULO areas */
+            public function count_areas(){
+                $sql = "SELECT COUNT(*) AS total FROM areas WHERE 1=1 ";
+                if(!empty($this->areas['skAreas'])){
+                    $sql .=" AND skAreas = '".$this->areas['skAreas']."'";
+                }
+                if(!empty($this->areas['nombre'])){
+                    $sql .=" AND nombre like '%".$this->areas['nombre']."%'";
+                }
+                if(!empty($this->areas['correo'])){
+                    $sql .=" AND correo like '%".$this->areas['correo']."%'";
+                }
+                if(!empty($this->areas['skStatus'])){
+                    $sql .=" AND skStatus like '%".$this->areas['skStatus']."%'";
+                }
+                $result = $this->db->query($sql);
+                if($result){
+                    if($result->num_rows > 0){
+                        return $result;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+            
+            public function read_equal_areas(){
+                $sql = "SELECT areas.*, _status.sName AS status, _status.sHtml AS htmlStatus FROM areas INNER JOIN _status ON _status.skStatus = areas.skStatus WHERE 1=1 ";
+                if(!empty($this->areas['skAreas'])){
+                    $sql .=" AND skAreas = '".$this->areas['skAreas']."'";
+                }
+                if(!empty($this->areas['nombre'])){
+                    $sql .=" AND nombre = '".$this->areas['nombre']."'";
+                }
+                if(!empty($this->areas['correo'])){
+                    $sql .=" AND correo = '".$this->areas['correo']."'";
+                }
+                if(!empty($this->areas['skStatus'])){
+                    $sql .=" AND skStatus = '".$this->areas['skStatus']."'";
+                }
+                if(is_int($this->users['limit'])){
+                    if(is_int($this->users['offset'])){
+                        $sql .= " LIMIT ".$this->users['offset']." , ".$this->users['limit'];
+                    }else{
+                        $sql .= " LIMIT ".$this->users['limit'];
+                    }
+                }
+                $result = $this->db->query($sql);
+                if($result){
+                    if($result->num_rows > 0){
+                        return $result;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+            
+            public function read_like_areas(){
+                $sql = "SELECT areas.*, _status.sName AS status, _status.sHtml AS htmlStatus FROM areas INNER JOIN _status ON _status.skStatus = areas.skStatus WHERE 1=1 ";
+                if(!empty($this->areas['skAreas'])){
+                    $sql .=" AND skAreas = '".$this->areas['skAreas']."'";
+                }
+                if(!empty($this->areas['nombre'])){
+                    $sql .=" AND nombre like '%".$this->areas['nombre']."%'";
+                }
+                if(!empty($this->areas['correo'])){
+                    $sql .=" AND correo like '%".$this->areas['correo']."%'";
+                }
+                if(!empty($this->areas['skStatus'])){
+                    $sql .=" AND skStatus like '%".$this->areas['skStatus']."%'";
+                }
+                if(is_int($this->users['limit'])){
+                    if(is_int($this->users['offset'])){
+                        $sql .= " LIMIT ".$this->users['offset']." , ".$this->users['limit'];
+                    }else{
+                        $sql .= " LIMIT ".$this->users['limit'];
+                    }
+                }
+                $result = $this->db->query($sql);
+                if($result){
+                    if($result->num_rows > 0){
+                        return $result;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+            
+            public function create_areas(){
+                $sql = "SELECT * FROM areas";
+                $result = $this->db->query($sql);
+                if($result){
+                    if($result->num_rows > 0){
+                        return $result;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+            
+            public function update_areas(){
+                $sql = "SELECT * FROM areas";
+                $result = $this->db->query($sql);
+                if($result){
+                    if($result->num_rows > 0){
+                        return $result;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+            public function delete_areas(){
+                $sql = "SELECT * FROM areas";
+                $result = $this->db->query($sql);
+                if($result){
+                    if($result->num_rows > 0){
+                        return $result;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+            /* TERMINA MODULO areas */
 	}
 ?>
