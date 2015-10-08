16426ce0315cab4a20927db4125b461fd5d83675
Luis Alberto Valdez Alvarez <luisalberto1192@gmail.com>
2015-10-07 22:48:50 -0500

    Inicio del Sistema

--- core/stage/dashboard.php
+++ core/stage/dashboard.php
@@ -1,1 +1,45 @@
-<h1>Dashboard</h1>
+<!--<div class="row">
+				<div class="col-md-3 col-sm-6">
+					<div class="circle-stat stat-block">
+						<div class="visual">
+							<input class="knobify" data-width="115" data-thickness=".2" data-skin="tron" data-displayprevious="true" value="+89" data-max="100" data-min="-100"/>
+						</div>
+						<div class="details">
+							<div class="title">
+								 <a href="#">Opciones</a>
+							</div>
+							 
+							
+						</div>
+					</div>
+				</div>
+</div>-->
+
+<div class="clearfix">
+			</div>
+			<div class="row ">
+			
+				<div class="col-md-12">
+				<center>
+				<img src="<?php echo SYS_URL; ?>core/assets/img/logo.png" style="width:300px;height:200px;margin-top:40px;" />
+				</center>
+				</div>
+			</div>
+			<!--<div class="row ">
+			
+				<div class="col-md-6 col-sm-6">
+					<div class="portlet">
+						<div class="portlet-title">
+							<div class="caption">
+								<i class="fa fa-bell"></i>Opciones
+							</div>
+							
+						</div>
+						<div class="portlet-body">
+							<div class="table-scrollable">
+							
+							</div>
+						</div>
+					</div>
+				</div>-->
+				
