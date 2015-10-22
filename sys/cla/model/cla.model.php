33601df1ce2b985f85495a0c0440c0676532b00f
Luis Alberto Valdez Alvarez <luisalberto1192@gmail.com>
2015-10-22 14:23:28 -0500

    clasificación Modelo

--- sys/cla/model/cla.model.php
+++ sys/cla/model/cla.model.php
@@ -9,6 +9,32 @@
             ,'limit'        =>  ''
             ,'offset'       =>  ''
         );
+        
+        public $cat_numeros_partes = array(
+             'skNumeroParte'  =>  ''
+            ,'sNombre'     =>  ''
+            ,'sDescripcion'     =>  ''
+            ,'skStatus'     =>  ''
+            ,'dFechaCrecion'     =>  ''
+            ,'skUsersCreacion'     =>  ''
+            ,'dFechaModificacion'     =>  ''
+            ,'skUsersModificacion'     =>  ''
+            ,'limit'        =>  ''
+            ,'offset'       =>  ''
+        );
+        // cat_numerosParte_fraccionesFraccion
+         public $numparfraran = array(
+             'skNumeroParte'  =>  ''
+            ,'sNombre'     =>  ''
+            ,'sDescripcion'     =>  ''
+            ,'skStatus'     =>  ''
+            ,'dFechaCrecion'     =>  ''
+            ,'skUsersCreacion'     =>  ''
+            ,'dFechaModificacion'     =>  ''
+            ,'skUsersModificacion'     =>  ''
+            ,'limit'        =>  ''
+            ,'offset'       =>  ''
+        );
 
         // PRIVATE VARIABLES //
             private $data = array();
@@ -24,8 +50,9 @@
         /* COMIENZA MODULO clasifiación arancelaria */
         
         /* COMIENZA cat_descripcionFraccion_archivos */
-        public function create_cat_descripcionFraccion_archivos(){
-            $sql = "INSERT INTO cat_descripcionFraccion_archivos (skFraccionArancelariaDescripcion,sArchivo,skStatus) VALUES ('".$this->areas['skFraccionArancelariaDescripcion']."','".$this->areas['sArchivo']."','".$this->areas['skStatus']."')";
+        public function create_numparfraran(){
+            $sql = "INSERT INTO cat_descripcionFraccion_archivos (skFraccionArancelariaDescripcion,sArchivo,skStatus) 
+            VALUES ('".$this->numparfraran['skFraccionArancelariaDescripcion']."','".$this->numparfraran['sArchivo']."','".$this->numparfraran['skStatus']."')";
             $result = $this->db->query($sql);
             if($result){
                 return $this->areas['skFraccionArancelariaDescripcion'];
@@ -36,5 +63,122 @@
         /* TERMINA cat_descripcionFraccion_archivos */
         
         /* TERMINA MODULO DE EMPRESAS clasifiación arancelaria */
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        /* COMIENZA create_cat_numeros_partes */
+        public function create_cat_numeros_partes(){
+            $sql = "INSERT INTO cat_numeros_partes (skNumeroParte,sNombre,sDescripcion,skStatus,dFechaCreacion,skUsersCreacion) 
+            VALUES ('".$this->cat_numeros_partes['skNumeroParte']."','".$this->cat_numeros_partes['sNombre']."','".$this->cat_numeros_partes['sDescripcion']."','".$this->cat_numeros_partes['skStatus'].",'".$this->cat_numeros_partes['dFechaCreacion']."','".$this->cat_numeros_partes['skStatus']."',CURRENT_TIMESTAMP,'".$this->cat_numeros_partes['skUsersCreacion']."')";
+            $result = $this->db->query($sql);
+            if($result){
+                return $this->cat_numeros_partes['skNumeroParte'];
+            }else{
+                return false;
+            }
+        }
+          
+          public function update_cat_numeros_partes(){
+                $sql = "UPDATE cat_numeros_partes SET ";
+                
+                if(!empty($this->cat_numeros_partes['sNombre'])){
+                    $sql .=" sNombre = '".$this->cat_numeros_partes['sNombre']."' ,";
+                }
+                 if(!empty($this->cat_numeros_partes['sDescripcion'])){
+                    $sql .=" sDescripcion = '".$this->cat_numeros_partes['sDescripcion']."' ,";
+                }
+                if(!empty($this->cat_numeros_partes['skStatus'])){
+                    $sql .=" skStatus = '".$this->cat_numeros_partes['skStatus']."' ,";
+                }
+                $sql .= " skNumeroParte = '".$this->cat_numeros_partes['skNumeroParte']."' WHERE skNumeroParte = '".$this->cat_numeros_partes['skNumeroParte']."' LIMIT 1";
+                $result = $this->db->query($sql);
+                if($result){
+                    return $this->cat_numeros_partes['skAreas'];
+                }else{
+                    return false;
+                }
+            }
+          /* TERMINA create_cat_numeros_partes */
+        
+        public function create_cat_numeros_partes(){
+            $sql = "INSERT INTO cat_numerosParte_fraccionesArancelarias (skFraccionArancelaria,skNumeroParte,sNombre,skStatus,dFechaCreacion,skUsersCreacion) 
+            VALUES ('".$this->numparfraran['skFraccionArancelaria']."','".$this->numparfraran['skNumeroParte']."','".$this->numparfraran['sNombre']."',
+            		'".$this->numparfraran['skStatus'].",'".$this->numparfraran['dFechaCreacion']."','".$this->numparfraran['skStatus']."',
+            		CURRENT_TIMESTAMP,'".$this->numparfraran['skUsersCreacion']."')";
+            $result = $this->db->query($sql);
+            if($result){
+                return $this->numparfraran['skFraccionArancelaria'];
+            }else{
+                return false;
+            }
+        }
+        
+        public function update_numparfraran(){
+                $sql = "UPDATE cat_numerosParte_fraccionesFraccion SET ";
+                
+                if(!empty($this->numparfraran['sNombre'])){
+                    $sql .=" sNombre = '".$this->numparfraran['sNombre']."' ,";
+                }
+                 if(!empty($this->numparfraran['sDescripcion'])){
+                    $sql .=" sDescripcion = '".$this->numparfraran['sDescripcion']."' ,";
+                }
+                if(!empty($this->numparfraran['skStatus'])){
+                    $sql .=" skStatus = '".$this->numparfraran['skStatus']."' ,";
+                }
+                $sql .= " skFraccionArancelaria = '".$this->numparfraran['skNumeroParte']."' WHERE skFraccionArancelaria = '".$this->numparfraran['skFraccionArancelaria']."' LIMIT 1";
+                $result = $this->db->query($sql);
+                if($result){
+                    return $this->numparfraran['skFraccionArancelaria'];
+                }else{
+                    return false;
+                }
+            }
+        
+         
+        
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 ?>
