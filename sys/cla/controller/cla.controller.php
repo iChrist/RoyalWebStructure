10cb4ee0aae9adab2e2e64fa9a6917530a976e15
iChrist <christianjimenezcjs@gmail.com>
2015-10-21 22:35:23 -0500

    modelo

--- sys/cla/controller/cla.controller.php
+++ sys/cla/controller/cla.controller.php
@@ -14,6 +14,15 @@
 
         /* COMIENZA MODULO clasifiación arancelaria */
         public function index(){
+            //echo 432234234234;
+            
+            $this->desArc['skFraccionArancelariaDescripcion'] = !empty($_POST['skFraccionArancelariaDescripcion']) ? $_POST['skFraccionArancelariaDescripcion'] : substr(md5(microtime()), 1, 32);
+            $this->create_cat_descripcionFraccion_archivos();
+            
+            $this->fraAraDes['skFraccionArancelariaDescripcion'] = !empty($_POST['skFraccionArancelariaDescripcion']) ? $_POST['skFraccionArancelariaDescripcion'] : substr(md5(microtime()), 1, 32);
+            $this->create_cat_fraccionesArancelarias_descripcionFraccion();
+            
+            $this->desArc['sNombre'] = utf8_decode($_POST['sNombre']);
             
         }
         /* TERMINA MODULO DE EMPRESAS clasifiación arancelaria */    
