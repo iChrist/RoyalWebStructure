b6a558b6b36a3e106fed91960cdcabf73d7021fd
Luis Alberto Valdez Alvarez <luisalberto1192@gmail.com>
2015-10-24 18:12:07 -0500

    Insercion de Clasificación Arancelaria

--- sys/cla/model/cla.model.php
+++ sys/cla/model/cla.model.php
@@ -24,19 +24,7 @@
             ,'limit'        =>  NULL
             ,'offset'       =>  NULL
         );
-        
-        public $cat_numeros_partes = array(
-             'skNumeroParte'  =>  ''
-            ,'sNombre'     =>  ''
-            ,'sDescripcion'     =>  ''
-            ,'skStatus'     =>  ''
-            ,'dFechaCrecion'     =>  ''
-            ,'skUsersCreacion'     =>  ''
-            ,'dFechaModificacion'     =>  ''
-            ,'skUsersModificacion'     =>  ''
-            ,'limit'        =>  ''
-            ,'offset'       =>  ''
-        );
+
         // cat_numerosParte_fraccionesFraccion
          public $numparfraran = array(
              'skNumeroParte'  =>  ''
@@ -54,7 +42,7 @@
         public $numPar = array(
              'skNumeroParte' =>  NULL
             ,'sNombre'    =>  NULL
-            ,'sDecripcion'  =>  NULL
+            ,'sDescripcion'  =>  NULL
             ,'skStatus' =>  NULL
             ,'dFechaCreacion'   =>  NULL
             ,'skUsersCreacion'  =>  NULL
@@ -98,8 +86,8 @@
             if(!empty($this->numPar['sNombre'])){
                 $sql .=" AND (sNombre like '%".$this->numPar['sNombre']."%' or TodosLosNombres like '%".$this->numPar['sNombre']."%')";
             }
-            if(!empty($this->numPar['sDecripcion'])){
-                $sql .=" AND (sDecripcion like '%".$this->numPar['sDecripcion']."%' or TodosLosNombres like '%".$this->numPar['sDecripcion']."%')";
+            if(!empty($this->numPar['sDescripcion'])){
+                $sql .=" AND (sDescripcion like '%".$this->numPar['sDescripcion']."%' or TodosLosNombres like '%".$this->numPar['sDescripcion']."%')";
             }
             if(!empty($this->numPar['skStatus'])){
                 $sql .=" AND skStatus like '%".$this->numPar['skStatus']."%'";
@@ -133,8 +121,8 @@
             if(!empty($this->numPar['sNombre'])){
                 $sql .=" AND (numPar.sNombre like '%".$this->numPar['sNombre']."%' or numPar.TodosLosNombres like '%".$this->numPar['sNombre']."%')";
             }
-            if(!empty($this->numPar['sDecripcion'])){
-                $sql .=" AND (numPar.sDecripcion like '%".$this->numPar['sDecripcion']."%' or numPar.TodasLasDescripciones like '%".$this->numPar['sDecripcion']."%')";
+            if(!empty($this->numPar['sDescripcion'])){
+                $sql .=" AND (numPar.sDescripcion like '%".$this->numPar['sDescripcion']."%' or numPar.TodasLasDescripciones like '%".$this->numPar['sDescripcion']."%')";
             }
             if(!empty($this->numPar['skStatus'])){
                 $sql .=" AND numPar.skStatus like '%".$this->numPar['skStatus']."%'";
@@ -170,9 +158,10 @@
         }
 
         public function create_cat_descripcionFraccion_archivos(){
-            $sql = "INSERT INTO cat_descripcionFraccion_archivos (skFraccionArancelariaDescripcion,sArchivo,skStatus) "
-                    . "VALUES ('".$this->desArc['skFraccionArancelariaDescripcion']."','".$this->desArc['sArchivo']."','".$this->desArc['skStatus']."')";
-            $result = $this->db->query($sql);echo '<hr>'.$sql;
+            $sql = "INSERT INTO cat_descripcionFraccion_archivos (skArchivoFraccionArancelaria,skFraccionArancelariaDescripcion,sArchivo,skStatus) "
+                    . "VALUES ('".$this->desArc['skArchivoFraccionArancelaria']."','".$this->desArc['skFraccionArancelariaDescripcion']."','".$this->desArc['sArchivo']."','AC')";
+            //echo $sql;
+            $result = $this->db->query($sql); 
             if($result){
                 return $this->desArc['skFraccionArancelariaDescripcion'];
             }else{
@@ -195,27 +184,23 @@
             $sql = "INSERT INTO cat_fraccionesArancelarias_descripcionFraccion ("
                     . "skFraccionArancelariaDescripcion,"
                     . "skFraccionArancelaria,"
-                    . "sDecripcion,"
+                    . "sDescripcion,"
                     . "sDescripcionIngles,"
-                    . "sModelo,"
-                    . "skStatus,"
+                     . "skStatus,"
                     . "dFechaCreacion,"
-                    . "skUsersCreacion,"
-                    . "dFechaModificacion,"
-                    . "skUsersModificacion"
-                    . ") VALUES ("
+                    . "skUsersCreacion"
+                     . ") VALUES ("
                     . "'".$this->fraAraDes['skFraccionArancelariaDescripcion']."',"
                     . "'".$this->fraAraDes['skFraccionArancelaria']."',"
-                    . "'".$this->fraAraDes['sDecripcion']."',"
+                    . "'".$this->fraAraDes['sDescripcion']."',"
                     . "'".$this->fraAraDes['sDescripcionIngles']."',"
-                    . "'".$this->fraAraDes['sModelo']."',"
-                    . "'".$this->fraAraDes['skStatus']."',"
-                    . "'".$this->fraAraDes['dFechaCreacion']."',"
-                    . "'".$this->fraAraDes['skUsersCreacion']."',"
-                    . "'".$this->fraAraDes['dFechaModificacion']."',"
-                    . "'".$this->fraAraDes['skUsersModificacion']."'"
-                    . ")";
-            $result = $this->db->query($sql);echo '<hr>'.$sql;
+                    . "'AC',"
+                    . "CURRENT_TIMESTAMP,"
+                    . "'".$this->fraAraDes['skUsersCreacion']."'"
+                     . ")";
+                 //  echo $sql;
+                  
+            $result = $this->db->query($sql);
             if($result){
                 return $this->fraAraDes['skFraccionArancelariaDescripcion'];
             }else{
@@ -225,7 +210,7 @@
         
         public function update_cat_fraccionesArancelarias_descripcionFraccion(){
             $sql = "UPDATE cat_fraccionesArancelarias_descripcionFraccion SET "
-                    . "sDecripcion = '".$this->fraAraDes['sDecripcion']."',"
+                    . "sDescripcion = '".$this->fraAraDes['sDescripcion']."',"
                     . "sDescripcionIngles = '".$this->fraAraDes['sDescripcionIngles']."',"
                     . "sModelo = '".$this->fraAraDes['sModelo']."',"
                     . "skStatus = '".$this->fraAraDes['skStatus']."',"
@@ -248,43 +233,43 @@
         
 
         /* COMIENZA create_cat_numeros_partes */
-        public function create_cat_numeros_partes(){
-            $sql = "INSERT INTO cat_numeros_partes (skNumeroParte,sNombre,sDescripcion,skStatus,dFechaCreacion,skUsersCreacion) 
+        public function create_cat_numeroParte(){
+            $sql = "INSERT INTO cat_numerosParte (skNumeroParte,sNombre,sDescripcion,skStatus,dFechaCreacion,skUsersCreacion) 
             VALUES 
-            ('".$this->cat_numeros_partes['skNumeroParte']."',
-            '".$this->cat_numeros_partes['sNombre']."',
-            '".$this->cat_numeros_partes['sDescripcion']."',
-            '".$this->cat_numeros_partes['skStatus']."',
+            ('".$this->numPar['skNumeroParte']."',
+            '".$this->numPar['sNombre']."',
+            '".$this->numPar['sDescripcion']."',
+            '".$this->numPar['skStatus']."',
              CURRENT_TIMESTAMP,
-            '".$this->cat_numeros_partes['skUsersCreacion']."'
+            '".$this->numPar['skUsersCreacion']."'
             )";
             
             //echo $sql;
-            die();
+           
             $result = $this->db->query($sql);
             if($result){
-                return $this->cat_numeros_partes['skNumeroParte'];
+                return $this->numPar['skNumeroParte'];
             }else{
                 return false;
             }
         }
           
           public function update_cat_numeros_partes(){
-                $sql = "UPDATE cat_numeros_partes SET ";
+                $sql = "UPDATE cat_numerosParte SET ";
                 
-                if(!empty($this->cat_numeros_partes['sNombre'])){
-                    $sql .=" sNombre = '".$this->cat_numeros_partes['sNombre']."' ,";
+                if(!empty($this->numPar['sNombre'])){
+                    $sql .=" sNombre = '".$this->numPar['sNombre']."' ,";
                 }
-                 if(!empty($this->cat_numeros_partes['sDescripcion'])){
-                    $sql .=" sDescripcion = '".$this->cat_numeros_partes['sDescripcion']."' ,";
+                 if(!empty($this->numPar['sDescripcion'])){
+                    $sql .=" sDescripcion = '".$this->numPar['sDescripcion']."' ,";
                 }
-                if(!empty($this->cat_numeros_partes['skStatus'])){
-                    $sql .=" skStatus = '".$this->cat_numeros_partes['skStatus']."' ,";
+                if(!empty($this->numPar['skStatus'])){
+                    $sql .=" skStatus = '".$this->numPar['skStatus']."' ,";
                 }
-                $sql .= " skNumeroParte = '".$this->cat_numeros_partes['skNumeroParte']."' WHERE skNumeroParte = '".$this->cat_numeros_partes['skNumeroParte']."' LIMIT 1";
+                $sql .= " skNumeroParte = '".$this->numPar['skNumeroParte']."' WHERE skNumeroParte = '".$this->numPar['skNumeroParte']."' LIMIT 1";
                 $result = $this->db->query($sql);
                 if($result){
-                    return $this->cat_numeros_partes['skAreas'];
+                    return $this->numPar['skAreas'];
                 }else{
                     return false;
                 }
@@ -294,9 +279,10 @@
         public function create_cat_numparfraran(){
             $sql = "INSERT INTO cat_numerosParte_fraccionesArancelarias (skFraccionArancelaria,skNumeroParte,sNombre,skStatus,dFechaCreacion,skUsersCreacion) 
             VALUES ('".$this->numparfraran['skFraccionArancelaria']."','".$this->numparfraran['skNumeroParte']."','".$this->numparfraran['sNombre']."',
-                    '".$this->numparfraran['skStatus'].",'".$this->numparfraran['dFechaCreacion']."','".$this->numparfraran['skStatus']."',
-                    CURRENT_TIMESTAMP,'".$this->numparfraran['skUsersCreacion']."')";
+                    '".$this->numparfraran['skStatus']."',CURRENT_TIMESTAMP,'".$this->numparfraran['skUsersCreacion']."')";
             $result = $this->db->query($sql);
+           // echo $sql;
+         
             if($result){
                 return $this->numparfraran['skFraccionArancelaria'];
             }else{
