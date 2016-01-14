8672db5be08ddb05cd59b2821a5d1fabeb2b8554
iChrist <christianjimenezcjs@gmail.com>
2016-01-13 23:34:56 -0600

    fac model 1er versión

--- sys/fac/model/fac.model.php
+++ sys/fac/model/fac.model.php
@@ -2,27 +2,27 @@
 	Class Fac_Model Extends Core_Model {
                 
             // PUBLIC VARIABLES //
-                public $facdat = array(
-                    'skFacturacion'=>null
-                    ,'sReferencia'=>null
-                    ,'dFechaFacturacion'=>null
-                    ,'sFolio'=>null
-                    ,'fImporte'=>null
-                    ,'fIva'=>null
-                    ,'fTotalFacturado'=>null
-                    ,'fGanancia'=>null
-                    ,'fAA'=>null
-                    ,'fPromotor1'=>null
-                    ,'fPromotor2'=>null
-                    ,'skUserCreacion'=>null
-                    ,'skUserModificacion'=>null
-                    ,'dFechaCreacion'=>null
-                    ,'dFechaModificacion'=>null
-                    ,'limit'=>null
-                    ,'offset'=>null
+            public $facdat = array(
+                'skFacturacion'=>null
+                ,'sReferencia'=>null
+                ,'dFechaFacturacion'=>null
+                ,'sFolio'=>null
+                ,'fImporte'=>null
+                ,'fIva'=>null
+                ,'fTotalFacturado'=>null
+                ,'fGanancia'=>null
+                ,'fAA'=>null
+                ,'fPromotor1'=>null
+                ,'fPromotor2'=>null
+                ,'skUserCreacion'=>null
+                ,'skUserModificacion'=>null
+                ,'dFechaCreacion'=>null
+                ,'dFechaModificacion'=>null
+                ,'limit'=>null
+                ,'offset'=>null
                 );
             // PRIVATE VARIABLES //
-                private $data = array();
+            private $data = array();
 
             public function __construct(){
                 parent::__construct();
@@ -31,122 +31,156 @@
 
             }
 			
-            public function count_solreva(){
-                $sql = "SELECT COUNT(*) AS total FROM ope_solicitud_revalidacion WHERE 1=1 ";
-                if(!empty($this->solreva['skSolicitudRevalidacion'])){
-                    $sql .=" AND skSolicitudRevalidacion = '".$this->solreva['skSolicitudRevalidacion']."'";
+            public function count_facdat(){
+                $sql = "SELECT COUNT(*) AS total FROM ope_facturacion AS facdat WHERE 1=1 ";
+                if(!is_null($this->facdat['skFacturacion'])){
+                    $sql .=" AND facdat.skFacturacion = '".$this->facdat['skFacturacion']."'";
                 }
-                if(!empty($this->solreva['sReferencia'])){
-                    $sql .=" AND sReferencia like '%".$this->solreva['sReferencia']."%'";
+                if(!is_null($this->facdat['sReferencia'])){
+                    $sql .=" AND facdat.sReferencia like '%".$this->facdat['sReferencia']."%'";
                 }
-                //echo $sql;die();
-                $result = $this->db->query($sql);
-                if($result){
-                    if($result->num_rows > 0){
-                        return $result;
-                    }else{
-                        return false;
-                    }
+                if(!is_null($this->facdat['dFechaFacturacion'])){
+                    $sql .=" AND facdat.dFechaFacturacion = '".$this->facdat['dFechaFacturacion']."'";
                 }
-            }
-            
-            public function read_solreva(){
-                $sql = "SELECT 	sd.*,
-                FROM ope_solicitud_revalidacion sd 
-                WHERE 1=1 ";
-                if(!empty($this->solreva['skSolicitudRevalidacion'])){
-                    $sql .=" AND sd.skSolicitudRevalidacion = '".$this->solreva['skSolicitudRevalidacion']."'";
+                if(!is_null($this->facdat['sFolio'])){
+                    $sql .=" AND facdat.sFolio like '%".$this->facdat['sFolio']."%'";
                 }
-                if(!empty($this->solreva['sReferencia'])){
-                    $sql .=" AND sd.sReferencia like '%".$this->solreva['sReferencia']."%'";
+                if(!is_null($this->facdat['fImporte'])){
+                    $sql .=" AND facdat.fImporte like '%".$this->facdat['fImporte']."%'";
                 }
-                //echo $sql;die();
-                $result = $this->db->query($sql);
-                if($result){
-                    if($result->num_rows > 0){
-                        return $result;
-                    }else{
-                        return false;
-                    }
+                if(!is_null($this->facdat['fIva'])){
+                    $sql .=" AND facdat.fIva like '%".$this->facdat['fIva']."%'";
                 }
-            }
-            
-            public function create_solreva(){
-                $sql = "INSERT INTO ope_solicitud_revalidacion 
-                    (skSolicitudRevalidacion,sReferencia,sObservaciones,skEmpresaNaviera,skEstatusRevalidacion,dFechaCreacion,skUsuarioSolicitud,sBL,iPrioridad,dFechaArriboBuque,dEta) 
-                    VALUES ('".$this->solreva['skSolicitudRevalidacion']."',
-                    '".$this->solreva['sReferencia']."',
-                    '".$this->solreva['sObservaciones']."',
-                    '".$this->solreva['skEmpresaNaviera']."',
-                    'NU',
-                    CURRENT_TIMESTAMP(),
-                    '".$_SESSION['session']['skUsers']."',
-                '".$this->solreva['sBL']."',
-                ".$this->solreva['iPrioridad'].",
-                '".$this->solreva['dFechaArriboBuque']."',
-                '".$this->solreva['dEta']."')";
-                //echo $sql;die();
-                $result = $this->db->query($sql);
-                if($result){
-                    return $this->solreva['skSolicitudRevalidacion'];
-                }else{
-                    return false;
+                if(!is_null($this->facdat['fTotalFacturado'])){
+                    $sql .=" AND facdat.fTotalFacturado like '%".$this->facdat['fTotalFacturado']."%'";
                 }
-            }
-             public function update_solreva(){
-                
-                $sql = "UPDATE ope_solicitud_revalidacion SET ";
-                
-                if(!is_null($this->solreva['skEmpresaNaviera'])){
-                    $sql.=" skEmpresaNaviera='".$this->solreva['skEmpresaNaviera']."', ";
+                if(!is_null($this->facdat['fGanancia'])){
+                    $sql .=" AND facdat.fGanancia like '%".$this->facdat['fGanancia']."%'";
                 }
-                if(!is_null($this->solreva['skUsuarioTramitador'])){
-                    $sql.=" skUsuarioTramitador='".$this->solreva['skUsuarioTramitador']."', ";
+                if(!is_null($this->facdat['fAA'])){
+                    $sql .=" AND facdat.fAA like '%".$this->facdat['fAA']."%'";
                 }
-                if(!is_null($this->solreva['dFechaProceso'])){
-                    $sql.=" dFechaProceso=".$this->solreva['dFechaProceso'].", ";
+                if(!is_null($this->facdat['fPromotor1'])){
+                    $sql .=" AND facdat.fPromotor1 like '%".$this->facdat['fPromotor1']."%'";
                 }
-                if(!is_null($this->solreva['dFechaCierre'])){
-                    $sql.=" dFechaCierre=".$this->solreva['dFechaCierre'].", ";
+                if(!is_null($this->facdat['fPromotor2'])){
+                    $sql .=" AND facdat.fPromotor2 like '%".$this->facdat['fPromotor2']."%'";
                 }
-                if(!is_null($this->solreva['sObservaciones'])){
-                    $sql.=" sObservaciones='".$this->solreva['sObservaciones']."', ";
+                if(!is_null($this->facdat['skUserCreacion'])){
+                    $sql .=" AND facdat.skUserCreacion = '".$this->facdat['skUserCreacion']."'";
                 }
-                if(!is_null($this->solreva['skEstatusRevalidacion'])){
-                    $sql.=" skEstatusRevalidacion='".$this->solreva['skEstatusRevalidacion']."', ";
+                if(!is_null($this->facdat['dFechaCreacion'])){
+                    $sql .=" AND facdat.dFechaCreacion = '".$this->facdat['dFechaCreacion']."'";
                 }
-                if(!is_null($this->solreva['skUsuarioProceso'])){
-                    $sql.=" skUsuarioProceso='".$this->solreva['skUsuarioProceso']."', ";
-                }
-                if(!is_null($this->solreva['skUsuarioCierre'])){
-                    $sql.=" skUsuarioCierre='".$this->solreva['skUsuarioCierre']."', ";
-                }
-                if(!is_null($this->solreva['sBL'])){
-                    $sql.=" sBL='".$this->solreva['sBL']."', ";
-                }
-                if(!is_null($this->solreva['iPrioridad'])){
-                    $sql.=" iPrioridad='".$this->solreva['iPrioridad']."', ";
-                }
-                if(!is_null($this->solreva['dFechaArriboBuque'])){
-                    $sql.=" dFechaArriboBuque='".$this->solreva['dFechaArriboBuque']."', ";
-                }
-                if(!is_null($this->solreva['dEta'])){
-                    $sql.=" dEta='".$this->solreva['dEta']."', ";
-                }
-                $sql .= " skSolicitudRevalidacion = '".$this->solreva['skSolicitudRevalidacion']."' WHERE skSolicitudRevalidacion = '".$this->solreva['skSolicitudRevalidacion']."'";
                 //exit($sql);
                 $result = $this->db->query($sql);
-                $sql = "DELETE from rel_solicitud_revalidaciones_rechazos  WHERE skSolicitudRevalidacion = '".$this->solreva['skSolicitudRevalidacion']."'";
-                $this->db->query($sql);
                 if($result){
-                    return $this->solreva['skSolicitudRevalidacion'];
+                    if($result->num_rows > 0){
+                        return $result;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+            
+            public function read_facdat(){
+                $sql = "SELECT 	facdat.*,
+                FROM ope_facturacion AS facdat
+                INNER JOIN ope_recepciones_documentos AS rd ON rd.sReferencia = facdat.sReferencia
+                INNER JOIN _users AS usr ON usr.skUsers =  facdat.skUserCreacion
+                WHERE 1=1 ";
+                if(!is_null($this->facdat['skFacturacion'])){
+                    $sql .=" AND facdat.skFacturacion = '".$this->facdat['skFacturacion']."'";
+                }
+                if(!is_null($this->facdat['sReferencia'])){
+                    $sql .=" AND facdat.sReferencia like '%".$this->facdat['sReferencia']."%'";
+                }
+                if(!is_null($this->facdat['dFechaFacturacion'])){
+                    $sql .=" AND facdat.dFechaFacturacion = '".$this->facdat['dFechaFacturacion']."'";
+                }
+                if(!is_null($this->facdat['sFolio'])){
+                    $sql .=" AND facdat.sFolio like '%".$this->facdat['sFolio']."%'";
+                }
+                if(!is_null($this->facdat['fImporte'])){
+                    $sql .=" AND facdat.fImporte like '%".$this->facdat['fImporte']."%'";
+                }
+                if(!is_null($this->facdat['fIva'])){
+                    $sql .=" AND facdat.fIva like '%".$this->facdat['fIva']."%'";
+                }
+                if(!is_null($this->facdat['fTotalFacturado'])){
+                    $sql .=" AND facdat.fTotalFacturado like '%".$this->facdat['fTotalFacturado']."%'";
+                }
+                if(!is_null($this->facdat['fGanancia'])){
+                    $sql .=" AND facdat.fGanancia like '%".$this->facdat['fGanancia']."%'";
+                }
+                if(!is_null($this->facdat['fAA'])){
+                    $sql .=" AND facdat.fAA like '%".$this->facdat['fAA']."%'";
+                }
+                if(!is_null($this->facdat['fPromotor1'])){
+                    $sql .=" AND facdat.fPromotor1 like '%".$this->facdat['fPromotor1']."%'";
+                }
+                if(!is_null($this->facdat['fPromotor2'])){
+                    $sql .=" AND facdat.fPromotor2 like '%".$this->facdat['fPromotor2']."%'";
+                }
+                if(!is_null($this->facdat['skUserCreacion'])){
+                    $sql .=" AND facdat.skUserCreacion = '".$this->facdat['skUserCreacion']."'";
+                }
+                if(!is_null($this->facdat['dFechaCreacion'])){
+                    $sql .=" AND facdat.dFechaCreacion = '".$this->facdat['dFechaCreacion']."'";
+                }
+                //exit($sql);
+                $result = $this->db->query($sql);
+                if($result){
+                    if($result->num_rows > 0){
+                        return $result;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+            
+            public function create_facdat(){
+                $sql = "INSERT INTO ope_facturacion 
+                    (skFacturacion,sReferencia,dFechaFacturacion,sFolio,fImporte,fIva,fTotalFacturado,fGanancia,fAA,fPromotor1,fPromotor2,skUserCreacion,dFechaCreacion)
+                    VALUES ( '".$this->facdat['skFacturacion']."',
+                    '".$this->facdat['sReferencia']."',
+                    '".$this->facdat['dFechaFacturacion']."',
+                    '".$this->facdat['sFolio']."',
+                    ".$this->facdat['fImporte'].",
+                    ".$this->facdat['fIva'].",
+                    ".$this->facdat['fTotalFacturado'].",
+                    ".$this->facdat['fGanancia'].",
+                    ".$this->facdat['fAA'].",
+                    ".$this->facdat['fPromotor1'].",
+                    ".$this->facdat['fPromotor2'].",
+                    '".$_SESSION['session']['skUsers']."',
+                    CURRENT_TIMESTAMP() )";
+                //exit($sql);
+                $result = $this->db->query($sql);
+                if($result){
+                    return $this->facdat['skFacturacion'];
+                }else{
+                    return false;
+                }
+            }
+             public function update_facdat(){
+                $sql = "UPDATE ope_facturacion SET ";
+                if(!is_null($this->facdat['sReferencia'])){
+                    $sql.=" sReferencia = '".$this->facdat['sReferencia']."', ";
+                }
+                $sql .= " skFacturacion = '".$this->facdat['skFacturacion']."' WHERE skFacturacion = '".$this->facdat['skFacturacion']."'";
+                //exit($sql);
+                $result = $this->db->query($sql);
+                if($result){
+                    return $this->facdat['skFacturacion'];
                 }else{
                     return false;
                 }
             }
             
-            public function delete_solreva(){
-                $sql = "DELETE FROM ope_solicitud_revalidacion WHERE skSolicitudRevalidacion = '".$this->solreva['skSolicitudRevalidacion']."' LIMIT 1";
+            public function delete_facdat(){
+                $sql = "UPDATE ope_facturacion SET skStatus = 'DE' WHERE skFacturacion IN (".implode(',',$this->facdat['skFacturacion']).")";
+                //exit($sql);
                 $result = $this->db->query($sql);
                 if($result){
                     return true;
