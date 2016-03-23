701ce49aaf5c054f680cc6536ac52a9fa5cdfeb3
Luis Alberto Valdez Alvarez <luisalberto1192@gmail.com>
2016-03-22 23:14:06 -0600

    Conceptos

--- sys/cof/controller/cof.controller.php
+++ sys/cof/controller/cof.controller.php
@@ -10,13 +10,13 @@
 		}
 
 		public function __destruct(){
-		
+
 		}
 
                 /* COMIENZA MODULO USUARIOS */
                 public function cof_usua_con(){
                     if(isset($_GET['axn']) && $_GET['axn'] == 'fetch_all'){
-                        
+
                         // PARAMETROS PARA FILTRADO //
                         if(isset($_POST['sName'])){
                             $this->users['sName'] = $_POST['sName'];
@@ -30,28 +30,28 @@
                         if(isset($_POST['skStatus'])){
                             $this->users['skStatus'] = $_POST['skStatus'];
                         }
-                        
+
                         // TOTAL DE REGISTROS EN LA TABLA //
                         $getTotal = parent::count_user()->fetch_assoc();
                         $iTotalRecords = $getTotal['total'];
                         // "LIMIT" TOTAL DE REGISTROS PARA MOSTRAR //
                         $iDisplayLength = intval($_REQUEST['length']);
-                        $iDisplayLength = ($iDisplayLength < 0) ? $iTotalRecords : $iDisplayLength; 
+                        $iDisplayLength = ($iDisplayLength < 0) ? $iTotalRecords : $iDisplayLength;
                         // "OFFSET" //
                         $iDisplayStart = intval($_REQUEST['start']);
                         // PAGINA //
                         $sEcho = intval($_REQUEST['draw']);
-                        
+
                         $this->users['limit'] = $iDisplayLength;
                         $this->users['offset'] = $iDisplayStart;
                         $this->data['users'] = parent::read_filter_user();
-                        
+
                         $records = array();
-                        $records["data"] = array(); 
+                        $records["data"] = array();
 
                         $end = $iDisplayStart + $iDisplayLength;
                         $end = $end > $iTotalRecords ? $iTotalRecords : $end;
-                        
+
                         if($this->data['users']){
                             while($row = $this->data['users']->fetch_assoc()){
                                 $actions = $this->printModulesButtons(2,array($row['skUsers']));
@@ -62,7 +62,7 @@
                                     ,utf8_encode(($row['sPassword']))
                                     ,utf8_encode($row['sHtml'])
                                     , !empty($actions['sHtml']) ? '<div class="dropdown" style="position: absolute;"><button aria-expanded="true" aria-haspopup="true" data-toggle="dropdown" id="dropdownMenu1" type="button" class="btn btn-default btn-xs dropdown-toggle">Acciones<span class="caret"></span></button><ul aria-labelledby="dropdownMenu1" class="dropdown-menu">'.utf8_encode($actions['sHtml']).'</ul></div>' : ''
-                                    
+
                                 );
                             }
                         }
@@ -76,7 +76,7 @@
                     $this->data['status'] = parent::read_status();
                     $this->load_view('cof-usua-con', $this->data);
                 }
-                
+
                 public function cof_usua_form(){
                     $this->data['message'] = '';
                     $this->data['response'] = true;
@@ -88,10 +88,10 @@
                         switch ($_POST['axn'])
                         {
                             case "validarEmail":
-                                // echo 'false'; -> Email no encontrado 
+                                // echo 'false'; -> Email no encontrado
                                 // echo 'true';  -> Email encontrado
-								
-                                $this->users['sEmail'] = htmlentities(($_POST['sEmail']));
+
+                $this->users['sEmail'] = htmlentities(($_POST['sEmail']));
 								$this->users['skUsersDistinto'] = htmlentities(($_POST['skUsers']));
                                 if(parent::read_user())
                                 {
@@ -103,9 +103,9 @@
                                 }
                                 exit;
                             break;
-                            
+
                             case "validarUserName":
-                                // echo 'false'; -> UserName no encontrado 
+                                // echo 'false'; -> UserName no encontrado
                                 // echo 'true';  -> UserName encontrado
                                 $this->users['sUserName'] = htmlentities(($_POST['sUserName']));
                                 $this->users['skUsersDistinto'] = htmlentities(($_POST['skUsers']));
@@ -121,7 +121,7 @@
                             break;
                         }
                     }
-                    
+
                     if($_POST){
                        			$this->users['skUsers'] 				= ($_POST['skUsers'] ? $_POST['skUsers'] : substr(md5(microtime()), 1, 32));
 								$this->users['sName'] 				= (utf8_decode($_POST['sName']));
@@ -131,8 +131,8 @@
 								$this->users['sEmail'] 				= (utf8_decode($_POST['sEmail']));
 								$this->users['sUserName']	 		= (utf8_decode($_POST['sUserName']));
 								$this->users['skStatus']				= (utf8_decode($_POST['skStatus']));
-                                                                $this->users['sGroup']				= utf8_decode(!empty($_POST['sGroup']) ? $_POST['sGroup'] : 'U');
-								
+                $this->users['sGroup']				= utf8_decode(!empty($_POST['sGroup']) ? $_POST['sGroup'] : 'U');
+
                         if($_POST['skUsers']){
 								if(parent::update_Users()){
                                 if(isset($_POST['skProfiles'])) // En esta parte guardaremos todos los perfiles seleccionados para el nuevo usuario.
@@ -168,7 +168,7 @@
                             }
                         }else{
 							if($this->users['skUsers']){
-								
+
 								if(parent::create_Users()){
 										$bandera = 1;
 									if(isset($_POST['skProfiles'])) // En esta parte guardaremos todos los perfiles seleccionados para el nuevo usuario.
@@ -189,7 +189,7 @@
 										}
 										$rRespuesta = parent::create_Users_profiles($valores);
 									}
-									
+
 									$this->data['response'] = true;
 									$this->data['message'] = 'Registro insertado con &eacute;xito.';
 									header('Content-Type: application/json');
@@ -205,17 +205,17 @@
                             }
                         }
                     }
-					
-					if(isset($_GET['p1'])){
+
+										if(isset($_GET['p1'])){
                         $this->users['skUsers'] = $_GET['p1'];
                         $this->data['datos'] = parent::read_user();
                         $this->data['perfilesusuarios'] = parent::read_user_profile();
                     }
-                    
+
                     $this->load_view('cof-usua-form', $this->data);
                     return true;
                 }
-                
+
                 public function cof_usua_det(){
                     $this->data['datos'] = false;
                     $this->data['profiles'] = parent::read_profile(); // Mandamos a llamar todos los perfiles para cargarlos en la vista
@@ -227,44 +227,44 @@
                     }
                     $this->load_view('cof-usua-det', $this->data);
                 }
-                
+
                 /* TERMINA MODULO USUARIOS */
-              
-			  
-			   
+
+
+
 	        public function cof_perf_con(){
 	        	 if(isset($_GET['axn']) && $_GET['axn'] == 'fetch_all'){
-                        
+
                         // PARAMETROS PARA FILTRADO //
                         if(isset($_POST['sName'])){
                             $this->profiles['sName'] = $_POST['sName'];
                         }
-                      
+
                         if(isset($_POST['skStatus'])){
                             $this->profiles['skStatus'] = $_POST['skStatus'];
                         }
-                        
+
                         // TOTAL DE REGISTROS EN LA TABLA //
                         $getTotal = parent::count_profile()->fetch_assoc();
                         $iTotalRecords = $getTotal['total'];
                         // "LIMIT" TOTAL DE REGISTROS PARA MOSTRAR //
                         $iDisplayLength = intval($_REQUEST['length']);
-                        $iDisplayLength = ($iDisplayLength < 0) ? $iTotalRecords : $iDisplayLength; 
+                        $iDisplayLength = ($iDisplayLength < 0) ? $iTotalRecords : $iDisplayLength;
                         // "OFFSET" //
                         $iDisplayStart = intval($_REQUEST['start']);
                         // PAGINA //
                         $sEcho = intval($_REQUEST['draw']);
-                        
+
                         $this->profiles['limit'] = $iDisplayLength;
                         $this->profiles['offset'] = $iDisplayStart;
                         $this->data['profiles'] = parent::read_filter_profile();
-                        
+
                         $records = array();
-                        $records["data"] = array(); 
+                        $records["data"] = array();
 
                         $end = $iDisplayStart + $iDisplayLength;
                         $end = $end > $iTotalRecords ? $iTotalRecords : $end;
-                        
+
                         if($this->data['profiles']){
                             while($row = $this->data['profiles']->fetch_assoc()){
                                $actions = $this->printModulesButtons(2,array($row['skProfiles']));
@@ -286,8 +286,8 @@
                     }
                     $this->data['status'] = parent::read_status();
                     $this->load_view('cof-perf-con', $this->data);
-	        
-				//$this->require_view(); 
+
+				//$this->require_view();
 				/*$this->data["perfiles"] = parent::read_profile();
                                 $this->load_view('cof-perf-con', $this->data);*/
 			}
@@ -296,24 +296,24 @@
                     $this->data['message'] = '';
                     $this->data['response'] = true;
                     $this->data['datos'] = false;
-                    
-                  
+
+
 
                     if($_POST){
   	                    	// $this->skProfiles 	= isset($_GET['p1']) ? $_GET['p1'] : $_POST['skProfiles'];
   	                    	 $this->profiles['skProfiles'] = !empty($_POST['skProfiles']) ? $_POST['skProfiles'] : substr(md5(microtime()), 1, 32);
   	                    	 $this->profiles['sName'] = htmlentities($_POST['sName'],ENT_QUOTES);
   	                    	 $this->profiles['skStatus'] = htmlentities($_POST['skStatus'],ENT_QUOTES);
- 	                        
-	                        
-	                    
+
+
+
 	                        if(empty($_POST['skProfiles'])){
 		                        if(parent::create_profile()){
-		                        
+
 		                        	foreach($_POST as $tCampo => $tValor){
-										if(strstr($tCampo,"skModule")&&$tValor){ 
+										if(strstr($tCampo,"skModule")&&$tValor){
 											$skModule = "'".$_POST["eSeccion".str_replace("skModule","",$tCampo)]."'";
-											
+
 											$seR	= (isset($_POST["R_".str_replace("skModule","",$tCampo)]) ? "'R'" : "NULL");
 											$seW	= (isset($_POST["W_".str_replace("skModule","",$tCampo)]) ? "'W'" : "NULL");
 											$seD	= (isset($_POST["D_".str_replace("skModule","",$tCampo)]) ? "'D'" : "NULL");
@@ -336,7 +336,7 @@
  		 											}
  		 											}
 									}
-		                        	
+
  	                                $this->data['response'] = true;
 	                                $this->data['message'] = 'Registro insertado con &eacute;xito.';
 	                                header('Content-Type: application/json');
@@ -350,21 +350,21 @@
 	                                return false;
 	                           }
 
-	                        
+
 	                        }else{
                             if(parent::update_profile()){
-                            	
+
 	                    	 	parent::delete_permission_profile($_POST['skProfiles']);
 	                    	 	foreach($_POST as $tCampo => $tValor){
-												if(strstr($tCampo,"skModule")&&$tValor){ 
+												if(strstr($tCampo,"skModule")&&$tValor){
 													$skModule = "'".$_POST["eSeccion".str_replace("skModule","",$tCampo)]."'";
 													$seR	= (isset($_POST["R_".str_replace("skModule","",$tCampo)]) ? "'R'" : "NULL");
 													$seW	= (isset($_POST["W_".str_replace("skModule","",$tCampo)]) ? "'W'" : "NULL");
 													$seD	= (isset($_POST["D_".str_replace("skModule","",$tCampo)]) ? "'D'" : "NULL");
 													$seA	= (isset($_POST["A_".str_replace("skModule","",$tCampo)]) ? "'A'" : "NULL");
-													
+
 													if(isset($_POST["R_".str_replace("skModule","",$tCampo)])){
-												
+
 													  $datos = "(".$skModule.",'".$_POST['skProfiles']."',".$seR.")";
 		 											   parent::createPermissions($datos);
 															//$result=mysqli_query($this->cxsis,$select);
@@ -386,7 +386,7 @@
 		 											}
 												}
 										}
-										
+
                                 $this->data['response'] = true;
                                 $this->data['message'] = 'Registro actualizado con &eacute;xito.';
                                 header('Content-Type: application/json');
@@ -399,7 +399,7 @@
                                 echo json_encode($this->data);
                                 return false;
                             }
-                     
+
                        }
                     }
                     if(!empty($_GET['p1'])){
@@ -407,9 +407,9 @@
                         $this->data['datos'] = parent::read_profile();
                     }
                     $this->load_view('cof-perf-form', $this->data);
-             
+
                  }
-		
-		  
+
+
 	}
 ?>
